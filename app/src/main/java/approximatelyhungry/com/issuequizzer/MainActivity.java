281790a8676b84dc4e6a4b36c978de709efb2618
Patrick Sheeran <ptsheeran@gmail.com>
2018-03-26 14:47:08 -0400

    Initial commit

---
+++ app/src/main/java/approximatelyhungry/com/issuequizzer/MainActivity.java
@@ -0,0 +1,13 @@
+package approximatelyhungry.com.issuequizzer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
